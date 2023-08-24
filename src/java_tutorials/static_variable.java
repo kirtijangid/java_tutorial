1e4eca1a41a1f6957734a0d8f4e3ea8cbe67bb95
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-24 20:56:11 +0530

    adding staticvariables

---
+++ src/java_tutorials/static_variable.java
@@ -0,0 +1,27 @@
+package java_tutorials;
+
+public class static_variable {
+		// static variable
+		static int a = m1();
+
+		// static block
+		static
+		{
+			System.out.println("Inside static block");
+		}
+
+		// static method
+		static int m1()
+		{
+			System.out.println("from m1");
+			return 20;
+		}
+
+		// static method(main !!)
+		public static void main(String[] args)
+		{
+			System.out.println("Value of a : " + a);
+			System.out.println("from main");
+		}
+	}
+
