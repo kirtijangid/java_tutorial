948b5505bcab985396ba2e10b3f1206928b0ff8d
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-27 00:10:07 +0530

    Multilevel inheritance

--- src/java_tutorials/Multilevel.java
+++ src/java_tutorials/Multilevel.java
@@ -1,52 +1,54 @@
 package java_tutorials;
 
-//class A{
+class X {
 
-//	int a = 2;
-//}
-//class B extends A{
-//	int b = 3;
-//	
-//}
-//public class Multilevel extends B {
-//
-//	public static void main(String[] args) {
-//		
-//		Multilevel s = new Multilevel();
-//		System.out.println("value of a = " + s.a);
-//		System.out.println("value of b = " + s.b);
-//	}
-//
-//}
+	int a = 2;
+}
+
+class Y extends X {
+	int b = 3;
+
+}
+
+public class Multilevel extends Y {
+
+	public static void main(String[] args) {
+
+		Multilevel s = new Multilevel();
+		System.out.println("value of a = " + s.a);
+		System.out.println("value of b = " + s.b);
+	}
+
+}
 
 // another example using methods and constructors
 
-class Toys {
-	public Toys() {
-		System.out.println("class Toys");
-	}
-
-	public void type() {
-		System.out.println("type = toys");
-	}
-}
-
-class Toys2 extends Toys {
-	public Toys2() {
-		System.out.println("class Toys2");
-	}
-
-	public void category() {
-		System.out.println("category = rubber");
-	}
-}
-
-public class Multilevel extends Toys2 {
-	public static void main(String[] args) {
-		Multilevel obj = new Multilevel();
-
-		obj.type();
-		obj.category();
-	}
-
-}
+//class Toys {
+//	public Toys() {
+//		System.out.println("class Toys");
+//	}
+//
+//	public void type() {
+//		System.out.println("type = toys");
+//	}
+//}
+//
+//class Toys2 extends Toys {
+//	public Toys2() {
+//		System.out.println("class Toys2");
+//	}
+//
+//	public void category() {
+//		System.out.println("category = rubber");
+//	}
+//}
+//
+//public class Multilevel extends Toys2 {
+//	public static void main(String[] args) {
+//		Multilevel obj = new Multilevel();
+//
+//		obj.type();
+//		obj.category();
+//	}
+//
+//}
