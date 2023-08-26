edf68082305e768fea525cc74bacf624e756b468
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-26 23:13:36 +0530

    adding another example using constructor & methods

--- src/java_tutorials/Multilevel.java
+++ src/java_tutorials/Multilevel.java
@@ -1,19 +1,69 @@
 package java_tutorials;
 
-class A{
-	int a = 2;
-}
-class B extends A{
-	int b = 3;
-	
-}
-public class Multilevel extends B {
+//class A{
+//	int a = 2;
+//}
+//class B extends A{
+//	int b = 3;
+//	
+//}
+//public class Multilevel extends B {
+//
+//	public static void main(String[] args) {
+//		
+//		Multilevel s = new Multilevel();
+//		System.out.println("value of a = " + s.a);
+//		System.out.println("value of b = " + s.b);
+//	}
+//
+//}
 
-	public static void main(String[] args) {
-		
-		Multilevel s = new Multilevel();
-		System.out.println("value of a = " + s.a);
-		System.out.println("value of b = " + s.b);
+
+                 // another example
+
+class Toys{
+	public Toys() {
+		System.out.println("class Toys");
 	}
-
+	public void type() {
+		System.out.println("type = toys");
+	}
 }
+	
+	class Toys2 extends Toys{
+		public Toys2() {
+			System.out.println("class Toys2");
+		}
+		public void category(){
+			System.out.println("category = rubber");
+		}
+	}
+	public class Multilevel extends Toys2{
+		public static void main(String[] args) {
+			Multilevel obj = new Multilevel();
+			
+			obj.type();
+			obj.category();
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
