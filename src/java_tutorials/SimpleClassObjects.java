96fbccf8b763a5c9873c0b9783a04119f3215e69
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-26 13:29:19 +0530

    adding ConstructorOverloading

--- src/java_tutorials/SimpleClassObjects.java
+++ src/java_tutorials/SimpleClassObjects.java
@@ -4,23 +4,26 @@ It is a template or blueprint from which objects are created. It is a logical en
 
 package java_tutorials;
 
- class SimpleClassObjects1 {
- 
-		 int id;  
-		 String name;  
-		}  
-		class SimpleClassObjects{  
-		 public static void main(String args[]){  
-		  //Creating objects  
-			 SimpleClassObjects1 s1=new SimpleClassObjects1();  
-			 SimpleClassObjects1 s2=new SimpleClassObjects1();  
-		  //Initializing objects  
-		  s1.id=101;  
-		  s1.name="kirti";  
-		  s2.id=102;  
-		  s2.name="suryansh";  
-		  //Printing data  
-		  System.out.println(s1.id+" "+s1.name);  
-		  System.out.println(s2.id+" "+s2.name);  
-		 }
-		}
+class SimpleClassObjects1 {
+
+	int id;
+	String name;
+}
+
+class SimpleClassObjects {
+	public static void main(String args[]) {
+
+		// Creating objects
+		SimpleClassObjects1 s1 = new SimpleClassObjects1();
+		SimpleClassObjects1 s2 = new SimpleClassObjects1();
+
+		// Initializing objects
+		s1.id = 101;
+		s1.name = "kirti";
+		s2.id = 102;
+		s2.name = "suryansh";
+
+		System.out.println(s1.id + " " + s1.name);
+		System.out.println(s2.id + " " + s2.name);
+	}
+}
