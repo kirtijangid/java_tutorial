0c7868d3e95f52603f7994f43214e3c29c4642ff
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-26 23:00:27 +0530

    Multilevel inheritance

--- src/java_tutorials/Multilevel.java
+++ src/java_tutorials/Multilevel.java
@@ -1,10 +1,19 @@
 package java_tutorials;
 
-public class Multilevel {
+class A{
+	int a = 2;
+}
+class B extends A{
+	int b = 3;
+	
+}
+public class Multilevel extends B {
 
 	public static void main(String[] args) {
 		
-
+		Multilevel s = new Multilevel();
+		System.out.println("value of a = " + s.a);
+		System.out.println("value of b = " + s.b);
 	}
 
 }
