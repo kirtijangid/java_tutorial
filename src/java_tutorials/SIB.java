7b4c45344dcd26e7c3861cb6774b71821ffc183a
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-26 20:25:37 +0530

    adding call for non-static members using constructors

--- src/java_tutorials/SIB.java
+++ src/java_tutorials/SIB.java
@@ -1,8 +1,8 @@
 package java_tutorials;
 
 public class SIB {
-	// SIB executes when the class gets loaded and executes only once in entire
-	// execution
+	// SIB executes when the class gets loaded and executes only once in entire execution
+	
 	static {
 		System.out.println("SIB-1");
 	}
@@ -10,8 +10,7 @@ public class SIB {
 	static {
 		System.out.println("SIB-2");
 	}
-	// IIB executes when the constructor is called but before the execution of
-	// constructor.
+	// IIB executes when the constructor is called but before the execution of constructor.
 	// So it executes as many times as constructor gets executed.
 	{
 		System.out.println("IIB-1");
