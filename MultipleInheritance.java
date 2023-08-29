1c9b6f78950f26e5f5bbe08d92bfa7e03812bf8f
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-30 02:11:01 +0530

    Method overloading

--- MultipleInheritance.java
+++ MultipleInheritance.java
@@ -1,18 +1,24 @@
 package java_tutorials;
 
-interface Drawable{  
-void draw();  
-}  
-  
-class Rectangle implements Drawable{  
-public void draw(){System.out.println("drawing rectangle");}  
-}  
-class Circle implements Drawable{  
-public void draw(){System.out.println("drawing circle");}  
-}  
+interface Drawable {
+	void draw();
+}
 
-class MultipleInheritance{  
-public static void main(String args[]){  
-Drawable d=new Circle();//In real scenario, object is provided by method e.g. getDrawable()  
-d.draw();  
-}}  
+class Rectangle implements Drawable {
+	public void draw() {
+		System.out.println("drawing rectangle");
+	}
+}
+
+class Circle implements Drawable {
+	public void draw() {
+		System.out.println("drawing circle");
+	}
+}
+
+class MultipleInheritance {
+	public static void main(String args[]) {
+		Drawable d = new Circle(); // object is provided by method e.g. getDrawable()
+		d.draw();
+	}
+}
