f6d2063b63696578549e0339d294f92c1021e985
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-29 22:53:54 +0530

    Example of an abstract class that has abstract and non-abstract methods

--- Abstract.java
+++ Abstract.java
@@ -1,6 +1,7 @@
 package java_tutorials;
 
 //abstract class Abstract1{
+
 //		abstract void run();     // abstract method don't have body
 //			
 //	
@@ -15,9 +16,8 @@ package java_tutorials;
 //	}
 //}
 
+// another example using abstract class and method
 
-				// another example using abstract class and method
-        
 //abstract class Shape{  
 //abstract void draw();  
 //}  
@@ -36,28 +36,53 @@ package java_tutorials;
 //}  
 //}  
 
+// another example for abstract class and method
 
-    			// another example for abstract class and method
+//abstract class Bank{    
+//abstract int getRateOfInterest();    
+//}    
+//class SBI extends Bank{    
+//int getRateOfInterest(){return 7;}    
+//}    
+//class PNB extends Bank{    
+//int getRateOfInterest(){return 8;}    
+//}    
+//    
+//class Abstract{    
+//public static void main(String args[]){    
+//Bank b;  
+//b=new SBI();  
+//System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");    
+//b=new PNB();  
+//System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");    
+//}}    
 
-abstract class Bank{    
-abstract int getRateOfInterest();    
-}    
-class SBI extends Bank{    
-int getRateOfInterest(){return 7;}    
-}    
-class PNB extends Bank{    
-int getRateOfInterest(){return 8;}    
-}    
-    
-class Abstract{    
-public static void main(String args[]){    
-Bank b;  
-b=new SBI();  
-System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");    
-b=new PNB();  
-System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");    
-}}    
+//Example of an abstract class that has abstract and non-abstract methods  
 
+abstract class Bike {
+	Bike() {
+		System.out.println("bike is created");
+	}
 
+	abstract void run();
 
+	void changeGear() {
+		System.out.println("gear changed");
+	}
+}
 
+//Creating a Child class which inherits Abstract class  
+class Honda extends Bike {
+	void run() {
+		System.out.println("running safely..");
+	}
+}
+
+//Creating a Test class which calls abstract and non-abstract methods  
+class Abstract {
+	public static void main(String args[]) {
+		Bike obj = new Honda();
+		obj.run();
+		obj.changeGear();
+	}
+}
