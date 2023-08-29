1077c8560a223cc47fd0f1cfbd72b99412b14a86
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-29 22:43:53 +0530

    another example using abstract class & method

--- Abstract.java
+++ Abstract.java
@@ -16,22 +16,48 @@ package java_tutorials;
 //}
 
 
+				// another example using abstract class and method
+        
+//abstract class Shape{  
+//abstract void draw();  
+//}  
+//  
+//class Rectangle extends Shape{  
+//void draw(){System.out.println("drawing rectangle");}  
+//}  
+//class Circle1 extends Shape{  
+//void draw(){System.out.println("drawing circle");}  
+//}  
+// 
+//class Abstract{  
+//public static void main(String args[]){  
+//Shape s=new Rectangle();  
+//s.draw();  
+//}  
+//}  
 
 
-abstract class Shape{  
-abstract void draw();  
-}  
-  
-class Rectangle extends Shape{  
-void draw(){System.out.println("drawing rectangle");}  
-}  
-class Circle1 extends Shape{  
-void draw(){System.out.println("drawing circle");}  
-}  
- 
-class Abstract{  
-public static void main(String args[]){  
-Shape s=new Rectangle();  
-s.draw();  
-}  
-}  
+    			// another example for abstract class and method
+
+abstract class Bank{    
+abstract int getRateOfInterest();    
+}    
+class SBI extends Bank{    
+int getRateOfInterest(){return 7;}    
+}    
+class PNB extends Bank{    
+int getRateOfInterest(){return 8;}    
+}    
+    
+class Abstract{    
+public static void main(String args[]){    
+Bank b;  
+b=new SBI();  
+System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");    
+b=new PNB();  
+System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");    
+}}    
+
+
+
+
