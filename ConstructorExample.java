d227dfb44299ac07c2ea2c77980cd4699e6b949c
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-30 13:17:32 +0530

    Constructor example

--- ConstructorExample.java
+++ ConstructorExample.java
@@ -1,39 +1,61 @@
 package java_tutorials;
 
-class TestConstructor {
-	// declare data member
-	int number;
-	String name;
+//class TestConstructor {
 
-	TestConstructor() {
-		System.out.println("Default constructor of TestConstructor class call.");
-	}
+//	// declare data member
+//	int number;
+//	String name;
+//
+//	TestConstructor() {
+//		System.out.println("Default constructor of TestConstructor class call.");
+//	}
+//
+//	TestConstructor(int number, String name) {
+//
+//		this.number = number;
+//		this.name = name;
+//		System.out.println("Parameterized constructor of TestConstructor class call.");
+//	}
+//}
+//
+//// create ConstructorExample class that invoke the constructor of TestConstructor  
+//public class ConstructorExample {
+//	// main() method start
+//	public static void main(String[] args) {
+//		// this would invoke the default constructor.
+//		TestConstructor obj1 = new TestConstructor();
+//
+//		// Default constructor always provide default values, i.e., 0 and null, to the
+//		// object
+//		System.out.println(obj1.name);
+//		System.out.println(obj1.number);
+//
+//		// this would invoke the parameterized constructor.
+//		TestConstructor obj2 = new TestConstructor(131, "Paul");
+//
+//		// parameterized constructor invoke the object with the given value
+//		System.out.println(obj2.name);
+//		System.out.println(obj2.number);
+//	}
+//}
 
-	TestConstructor(int number, String name) {
+// Another Constructor example 
 
-		this.number = number;
-		this.name = name;
-		System.out.println("Parameterized constructor of TestConstructor class call.");
-	}
-}
-
-// create ConstructorExample class that invoke the constructor of TestConstructor  
 public class ConstructorExample {
-	// main() method start
-	public static void main(String[] args) {
-		// this would invoke the default constructor.
-		TestConstructor obj1 = new TestConstructor();
+	int num;
 
-		// Default constructor always provide default values, i.e., 0 and null, to the
-		// object
-		System.out.println(obj1.name);
-		System.out.println(obj1.number);
+	ConstructorExample() {
+		num = 3;
+		System.out.println("Constructor invoked. num: " + num);
+	}
 
-		// this would invoke the parameterized constructor.
-		TestConstructor obj2 = new TestConstructor(131, "Paul");
+	public void init() {
+		num = 5;
+		System.out.println("Method invoked. num: " + num);
+	}
 
-		// parameterized constructor invoke the object with the given value
-		System.out.println(obj2.name);
-		System.out.println(obj2.number);
+	public static void main(String args[]) {
+		ConstructorExample tester = new ConstructorExample();
+		tester.init();
 	}
 }
