90d3e92c5f1e8e941477a85f89302a521eaf2a62
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-30 14:54:51 +0530

    Multiple inheritance using interfaces

--- MultipleInheritance.java
+++ MultipleInheritance.java
@@ -1,24 +1,43 @@
 package java_tutorials;
 
-interface Drawable {
-	void draw();
-}
+interface Printable{  
+void print();  
+}  
+interface Showable{  
+void print();  
+}  
+  
+class MultipleInheritance implements Printable, Showable{  
+public void print(){System.out.println("Hello");}  
+public static void main(String args[]){  
+MultipleInheritance obj = new MultipleInheritance();  
+obj.print();  
+ }  
+}  
 
-class Rectangle implements Drawable {
-	public void draw() {
-		System.out.println("drawing rectangle");
-	}
-}
 
-class Circle implements Drawable {
-	public void draw() {
-		System.out.println("drawing circle");
-	}
-}
 
-class MultipleInheritance {
-	public static void main(String args[]) {
-		Drawable d = new Circle(); // object is provided by method e.g. getDrawable()
-		d.draw();
-	}
-}
+
+
+//interface Drawable {
+//	void draw();
+//}
+//
+//class Rectangle implements Drawable {
+//	public void draw() {
+//		System.out.println("drawing rectangle");
+//	}
+//}
+//
+//class Circle implements Drawable {
+//	public void draw() {
+//		System.out.println("drawing circle");
+//	}
+//}
+//
+//class MultipleInheritance {
+//	public static void main(String args[]) {
+//		Drawable d = new Circle(); // object is provided by method e.g. getDrawable()
+//		d.draw();
+//	}
+//}
---
+++ Permutation.java
@@ -0,0 +1,7 @@
+package java_tutorials;
+
+public class Permutation {
+	public static void main(String[] args) {
+		
+	}
+}
