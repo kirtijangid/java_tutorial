41ecc00f0d6c903327bf6e7ab074a1fce66a6ad3
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-30 23:46:21 +0530

    Explicit narrowing

--- AutoWidening.java
+++ AutoWidening.java
@@ -1,15 +1,24 @@
 package java_tutorials;
 
-public class AutoWidening{
+public class AutoWidening {
 
 	public static void main(String[] args) {
 
 		int i = 100;
-		long l = i; // no explicit type casting is required
-		float f = l; // no explicit type casting required
+		long l = i;
+		float f = l;
 		System.out.println("i= " + i);
 		System.out.println("l= " + l);
 		System.out.println("f= " + f);
 	}
-	
+
 }
+
+
+			// Examples:
+
+			//short=byte
+			//int=short
+			//long=int
+			//float=long
+			//double=float
---
+++ ExplicitNarrowing.java
@@ -0,0 +1,18 @@
+package java_tutorials;
+
+public class ExplicitNarrowing {
+	 
+    public static void main(String[] args) {
+ 
+        double d = 100.01;
+               // long l=d;  compile time error as we are assigning larger type to smaller type without casting
+        long l = (long) d; // explicit type casting is required
+        int i = (int) l; // explicit type casting is required
+ 
+        System.out.println("i= " + i);
+        System.out.println("l= " + l);
+        System.out.println("d= " + d);
+ 
+    }
+ 
+}
