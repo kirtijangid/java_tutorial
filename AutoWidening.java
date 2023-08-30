ba9e13d5431fcdc21d004d4520eadbec20c1ae36
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-30 23:54:56 +0530

    Auto widening/implicit widening

--- AutoWidening.java
+++ AutoWidening.java
@@ -7,6 +7,7 @@ public class AutoWidening {
 		int i = 100;
 		long l = i;
 		float f = l;
+		
 		System.out.println("i= " + i);
 		System.out.println("l= " + l);
 		System.out.println("f= " + f);
@@ -22,3 +23,19 @@ public class AutoWidening {
 			//long=int
 			//float=long
 			//double=float
+
+
+//public class AutoWidening {
+//	  static void myMethod(Integer num){
+//	    System.out.println("Integer Wrapper class type");
+//	  }
+//
+//	  static void myMethod(double num){
+//	    System.out.println("double primitive type");
+//	  }
+//	  public static void main(String[] args) {
+//	    //primitive int type
+//	    int n = 100;
+//	    myMethod(n);
+//	  }
+//	}
