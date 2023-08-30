eb820a5f3ee7f9df896111cb143128ffeffaf929
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-30 15:12:52 +0530

    inheriting using interface

--- TestInterface.java
+++ TestInterface.java
@@ -1,23 +1,48 @@
 package java_tutorials;
 
-interface First{
-	void show();
-}
-interface Second extends First{
-	void shows();
+//interface First{
 
+//	void show();
+//}
+//interface Second extends First{
+//	void shows();
+//
+//}
+// class TestInterface implements  Second{
+//	
+//		public void show() {
+//			System.out.println("hello");
+//		}
+//		public void shows() {
+//			System.out.println("World!");
+//		}
+//		public static void main(String[] args) {
+//			TestInterface obj = new TestInterface();
+//			obj.show();
+//			obj.shows();
+//	}
+//}
+
+// implements using interfaces
+
+interface Drawable {
+	void draw();
+
+	default void msg() {
+		System.out.println("default method");
+	}
 }
- class TestInterface implements  Second{
-	
-		public void show() {
-			System.out.println("hello");
-		}
-		public void shows() {
-			System.out.println("World!");
-		}
-		public static void main(String[] args) {
-			TestInterface obj = new TestInterface();
-			obj.show();
-			obj.shows();
+
+class Rectangle implements Drawable {
+	public void draw() {
+		System.out.println("drawing rectangle");
+	}
+}
+
+class TestInterface {
+	public static void main(String args[]) {
+		Drawable d = new Rectangle();
+		d.draw();
+		d.msg();
 	}
 }
