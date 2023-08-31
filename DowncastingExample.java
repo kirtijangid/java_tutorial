b0675965da4cab74003262c6293ead209919b936
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-08-31 08:17:05 +0530

    Downcasting

--- DowncastingExample.java
+++ DowncastingExample.java
@@ -1,43 +1,37 @@
 package java_tutorials;
 
-class First {   
-    String name;   
-    
-    // A method which prints the data of the parent class   
-    void showMessage()   
-    {   
-        System.out.println("Parent method is called");   
-    }   
-}   
-    
-// Child class   
-class Second extends First {   
-    int age;   
-    
-    // Performing overriding  
-    @Override  
-    void showMessage()  
-    {   
-        System.out.println("Child method is called");   
-    }   
-}   
-    
-public class DowncastingExample{  
-    
-    public static void main(String[] args)   
-    {   
-        First p = new Second();  
-        p.name = "Shubham";  
-          
-        // Performing Downcasting Implicitly   
-        //Child c = new Parent(); // it gives compile-time error   
-          
-        // Performing Downcasting Explicitly   
-        Second c = (Second)p;   
-    
-        c.age = 18;   
-        System.out.println(c.name);   
-        System.out.println(c.age);   
-        c.showMessage();   
-    }   
-}  
+class First {
+	String name;
+
+	void showMessage() {
+		System.out.println("Parent method is called");
+	}
+}
+
+class Second extends First {
+	int age;
+
+	@Override
+	void showMessage() {
+		System.out.println("Child method is called");
+	}
+}
+
+public class DowncastingExample {
+
+	public static void main(String[] args) {
+		First p = new Second();
+		p.name = "Kirti";
+
+		// Performing Downcasting Implicitly
+		// Child c = new Parent(); // it gives compile-time error
+
+		// Performing Downcasting Explicitly
+		Second c = (Second) p;
+
+		c.age = 22;
+		System.out.println(c.name);
+		System.out.println(c.age);
+		c.showMessage();
+	}
+}
