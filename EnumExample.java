9d054dcc6432e8a87814ef353f53a0b8927ea9b0
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-01 12:50:14 +0530

    Enum example

--- EnumExample.java
+++ EnumExample.java
@@ -3,21 +3,48 @@ package java_tutorials;
   /* An enum is a special "class" that represents a group of constants 
 (unchangeable variables, like final variables).*/
 
-public class EnumExample {
+//public class EnumExample {
+//
+//	enum level{
+//		LOW,
+//		HIGH,
+//		MEDIUM
+//		
+//	}
+//	
+//	public static void main(String[] args) {
+//		level obj = level.MEDIUM;
+//		level obj1 = level.HIGH;
+//		
+//		System.out.println(obj);
+//		System.out.println(obj1);
+//	}
+//}
+//
 
-	enum level{
-		LOW,
-		HIGH,
-		MEDIUM
-		
-	}
-	
-	public static void main(String[] args) {
-		level obj = level.MEDIUM;
-		level obj1 = level.HIGH;
-		
-		System.out.println(obj);
-		System.out.println(obj1);
-	}
-}
+     // another example
 
+
+enum Level {
+	  LOW,
+	  MEDIUM,
+	  HIGH
+	}
+
+	public class EnumExample {
+	  public static void main(String[] args) {
+	    Level myVar = Level.MEDIUM;
+
+	    switch(myVar) {
+	      case LOW:
+	        System.out.println("Low level");
+	        break;
+	      case MEDIUM:
+	         System.out.println("Medium level");
+	        break;
+	      case HIGH:
+	        System.out.println("High level");
+	        break;
+	    }
+	  }
+	}
