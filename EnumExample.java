d30a1b9e510913b33c2bbde801976868ba7a50c8
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-01 12:48:02 +0530

    MethodOverridingPolymorphism.java

--- EnumExample.java
+++ EnumExample.java
@@ -1,5 +1,8 @@
 package java_tutorials;
 
+  /* An enum is a special "class" that represents a group of constants 
+(unchangeable variables, like final variables).*/
+
 public class EnumExample {
 
 	enum level{
--- MethodOverridingPolymorphism.java
+++ MethodOverridingPolymorphism.java
@@ -1,39 +1,62 @@
 package java_tutorials;
 
-class SuperParent {
+//class SuperParent {
 
-	void Print() {
+//
+//	void Print() {
+//
+//		System.out.println("parent class");
+//	}
+//}
+//
+//class subclass1 extends SuperParent {
+//
+//	void Print() {
+//		System.out.println("subclass1");
+//	}
+//}
+//
+//class subclass2 extends SuperParent {
+//
+//	void Print() {
+//
+//		System.out.println("subclass2");
+//	}
+//}
+//
+//class MethodOverridingPolymorphism {
+//
+//	public static void main(String[] args) {
+//
+//		// Creating object of class 1
+//		SuperParent a;
+//
+//		a = new subclass1();
+//		a.Print();
+//
+//		a = new subclass2();
+//		a.Print();
+//	}
+//}
 
-		System.out.println("parent class");
+// Another example for method overriding 
+
+class Vehicle {
+	
+	void run() {
+		System.out.println("Vehicle is moving");
 	}
 }
 
-class subclass1 extends SuperParent {
 
-	void Print() {
-		System.out.println("subclass1");
+class MethodOverridingPolymorphism extends Vehicle {
+	
+	void run() {
+		System.out.println("car is running safely");
 	}
-}
 
-class subclass2 extends SuperParent {
-
-	void Print() {
-
-		System.out.println("subclass2");
-	}
-}
-
-class MethodOverridingPolymorphism {
-
-	public static void main(String[] args) {
-
-		// Creating object of class 1
-		SuperParent a;
-
-		a = new subclass1();
-		a.Print();
-
-		a = new subclass2();
-		a.Print();
+	public static void main(String args[]) {
+		MethodOverridingPolymorphism obj = new MethodOverridingPolymorphism();
+		obj.run();
 	}
 }
