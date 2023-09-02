2ac73fc8bf91ae32e848a19dc57d08110c3c2c73
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-02 17:56:23 +0530

    Program to Illustrate Anonymous Inner Classes

--- AnonymousInnerClass.java
+++ AnonymousInnerClass.java
@@ -3,26 +3,50 @@ package java_tutorials;
 /* Program to Illustrate Anonymous Inner classes declaration Without 
 any Name as a subclass of the specified type*/
 
-class Demo {
-	void show()
-	{
-		System.out.println("i am in show method of super class");
-	}
+//class Demo {
+//	void show()
+//	{
+//		System.out.println("i am in show method of super class");
+//	}
+//}
+//class AnonymousInnerclass  {
+//	// An anonymous class with Demo as base class
+//	static Demo d = new Demo() {
+//		void show()
+//		{
+//			// Calling method show() via super keyword which refers to parent class
+//			super.show();
+//			System.out.println("i am in Anaonymous class");
+//		}
+//	};
+//	public static void main(String[] args)
+//	{
+//		// Calling show() method inside main() method
+//		d.show();
+//	}
+//}
+
+
+        /*Java Program to Illustrate Anonymous Inner Classes
+           Declaration Without Any Name as an implementer of Specified interface. */
+
+interface Hello {
+	void show();
 }
-class AnonymousInnerclass  {
-	// An anonymous class with Demo as base class
-	static Demo d = new Demo() {
-		void show()
+class AnonymousInnerClass {
+
+	// Class implementing interface
+	static Hello h = new Hello() {
+	
+		public void show()
 		{
-			// Calling method show() via super keyword which refers to parent class
-			super.show();
-			System.out.println("i am in Anaonymous class");
+			System.out.println("i am in anonymous class");
 		}
 	};
+
 	public static void main(String[] args)
 	{
 		// Calling show() method inside main() method
-		d.show();
+		h.show();
 	}
 }
-
