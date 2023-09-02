22704ca69917c0c8951b28a8da9d5d630ad77317
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-02 16:13:43 +0530

    calling predefined methods with static import

--- StaticImport.java
+++ StaticImport.java
@@ -1,11 +1,27 @@
 package java_tutorials;
 
-public class StaticImport {
+          //calling of predefined methods without static import
 
-	public static void main(String[] args) {
-		System.out.println(Math.sqrt(4));
-		System.out.println(Math.sqrt(9));
-		System.out.println(Math.sqrt(16));
+//public class StaticImport {
+//
+//	public static void main(String[] args) {
+//		System.out.println(Math.sqrt(4));
+//		System.out.println(Math.pow(9,2));
+//		System.out.println(Math.abs(16.9));
+//	}
+//
+//}
+
+
+       
+         //calling of predefined methods with static import
+
+import static java.lang.Math.*;
+class StaticImport {
+	public static void main(String[] args)
+	{
+		System.out.println(sqrt(4));
+		System.out.println(pow(2, 2));
+		System.out.println(abs(6.3));
 	}
-
 }
