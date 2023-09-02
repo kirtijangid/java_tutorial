e6b198ba21ba669fc2450270be0d04c93d87a731
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-02 13:02:43 +0530

    static import

---
+++ StaticImport.java
@@ -0,0 +1,11 @@
+package java_tutorials;
+
+public class StaticImport {
+
+	public static void main(String[] args) {
+		System.out.println(Math.sqrt(4));
+		System.out.println(Math.sqrt(9));
+		System.out.println(Math.sqrt(16));
+	}
+
+}
