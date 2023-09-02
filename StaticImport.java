7f18f0b3633a2e208a701c386392978525d9ba12
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-02 16:19:25 +0530

    calling of static member of System class without Class name

--- StaticImport.java
+++ StaticImport.java
@@ -1,6 +1,6 @@
 package java_tutorials;
 
-          //calling of predefined methods without static import
+//calling of predefined methods without static import
 
 //public class StaticImport {
 //
@@ -13,15 +13,31 @@ package java_tutorials;
 //}
 
 
-       
-         //calling of predefined methods with static import
+            //calling of predefined methods with static import
 
+//import static java.lang.Math.*;
+//
+//class StaticImport {
+//	public static void main(String[] args) {
+//		System.out.println(sqrt(4));
+//		System.out.println(pow(2, 2));
+//		System.out.println(abs(6.3));
+//	}
+//}
+
+
+        // calling of static member of System class without Class name
 import static java.lang.Math.*;
+import static java.lang.System.*;
 class StaticImport {
 	public static void main(String[] args)
 	{
-		System.out.println(sqrt(4));
-		System.out.println(pow(2, 2));
-		System.out.println(abs(6.3));
+		// We are calling static member of System class
+		// directly without System class name
+		out.println(sqrt(4));
+		out.println(pow(2, 2));
+		out.println(abs(6.3));
 	}
 }
+
+
