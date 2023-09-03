1c496092163fd6f772b4702f88178acede6e282c
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-03 11:32:27 +0530

    For each loop

---
+++ ForEachLoop.java
@@ -0,0 +1,17 @@
+package java_tutorials;
+
+
+//print array elements 
+
+class ForEachLoop {
+public static void main(String[] args) {
+   
+ // create an array
+ int[] numbers = {3, 9, 5, -5};
+ 
+ // for each loop 
+ for (int number: numbers) {
+   System.out.println(number);
+ }
+}
+}
